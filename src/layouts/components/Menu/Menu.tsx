f989dfd45a1279cfa02b963684aed557acf384a3
issgy <xjmgsqg@163.com>
2023-09-04 14:53:14 +0800

    Completed the left menu display of Layout 1

--- src/layouts/components/Menu/Menu.tsx
+++ src/layouts/components/Menu/Menu.tsx
@@ -18,7 +18,7 @@ interface IMenuProps {
 
 const renderMenuItems = (menu: IMenuItem[]) =>
   menu.map((item) => {
-    // react router v5或更早用useHistory。useNavigate()是一个函数，而useHistory是一个对象
+    //react router v5或更早用useHistory。useNavigate()是一个函数，而useHistory是一个对象
     const navigate = useNavigate();
     const { key, label, Icon, children, path } = item;
     //   没有子菜单 or 有子菜单但长度为0
@@ -47,7 +47,6 @@ const renderMenuItems = (menu: IMenuItem[]) =>
 
 export default memo((props: IMenuProps) => {
   const location = useLocation();
-  console.log(location);
   const globalState = useAppSelector(selectGlobal);
   const bottomText = globalState.collapsed ? globalState.version : `TDesign Starter ${globalState.version}`;
   const Logo = globalState.collapsed ? <MenuLogoMini /> : <MenuLogo />;
