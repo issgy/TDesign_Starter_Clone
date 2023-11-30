6f842b6d59ecd463d653b7d7e01808bf27652ef6
issgy <xjmgsqg@163.com>
2023-11-30 12:46:18 +0800

    feat:增加卡片列表页

--- src/pages/List/Card/components/ProductCard.tsx
+++ src/pages/List/Card/components/ProductCard.tsx
@@ -11,54 +11,52 @@ const CardIcon = React.memo(() => {
   const Icon = icons[random];
   return <Icon />;
 });
-const ProductCard = ({ product }: { product: IProduct }) => {
-  return (
-    <Card
-      className={Style.panel}
-      actions={
-        product.isSetup ? (
-          <Tag theme='success'>已启用</Tag>
-        ) : (
-          <Tag theme='default' disabled={true}>
-            已停用
-          </Tag>
-        )
-      }
-      avatar={
-        <Avatar size='56px'>
-          <CardIcon />
-        </Avatar>
-      }
-      footer={
-        <div className={Style.footer}>
-          <AvatarGroup cascading='left-up'>
-            <Avatar>{String.fromCharCode(64 + product.type || 0)}</Avatar>
-            <Avatar>+</Avatar>
-          </AvatarGroup>
-          <Dropdown
-            trigger='click'
-            options={[
-              {
-                content: '管理',
-                value: 1,
-              },
-              {
-                content: '删除',
-                value: 2,
-              },
-            ]}
-          >
-            <Button theme='default' variant='text' disabled={!product.isSetup}>
-              <Icon name='more' size={16} />
-            </Button>
-          </Dropdown>
-        </div>
-      }
-    >
-      <div className={Style.name}>{product?.name}</div>
-      <div className={Style.description}>{product?.description}</div>
-    </Card>
-  );
-};
+const ProductCard = ({ product }: { product: IProduct }) => (
+  <Card
+    className={Style.panel}
+    actions={
+      product.isSetup ? (
+        <Tag theme='success'>已启用</Tag>
+      ) : (
+        <Tag theme='default' disabled={true}>
+          已停用
+        </Tag>
+      )
+    }
+    avatar={
+      <Avatar size='56px'>
+        <CardIcon />
+      </Avatar>
+    }
+    footer={
+      <div className={Style.footer}>
+        <AvatarGroup cascading='left-up'>
+          <Avatar>{String.fromCharCode(64 + product.type || 0)}</Avatar>
+          <Avatar>+</Avatar>
+        </AvatarGroup>
+        <Dropdown
+          trigger='click'
+          options={[
+            {
+              content: '管理',
+              value: 1,
+            },
+            {
+              content: '删除',
+              value: 2,
+            },
+          ]}
+        >
+          <Button theme='default' variant='text' disabled={!product.isSetup}>
+            <Icon name='more' size={16} />
+          </Button>
+        </Dropdown>
+      </div>
+    }
+  >
+    <div className={Style.name}>{product?.name}</div>
+    <div className={Style.description}>{product?.description}</div>
+  </Card>
+);
 
 export default React.memo(ProductCard);
